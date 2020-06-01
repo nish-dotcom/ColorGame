7886b33fe1381db60d587c8e6763a36326f87dd2
Nish Shah <66185878+nish-dotcom@users.noreply.github.com>
2020-06-01 15:52:44 -0400

    add colorgame

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+app.get("/",function(req,res){
+  res.sendFile(path.join(__dirname+"/colorGame.html"));
+});
 
 app.get("/colorGame.css", function(req, res) {
     res.sendFile(__dirname + "/colorGame.css");
@@ -8,15 +11,12 @@ app.get("/colorGame.css", function(req, res) {
 app.get("/colorGame.js", function(req, res) {
     res.sendFile(__dirname + "/colorGame.js");
   });
-app.get("/",function(req,res){
-  res.sendFile(path.join(__dirname+"/colorGame.html"));
+
+// app.listen(process.env.PORT, process.env.IP, function(){
+//     console.log("The Server Has Started!");
+//  });
+
+app.listen(3000, function(req,res){
+	console.log("The Server has Started!")	   
 });
-
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("The Server Has Started!");
- });
-
-// app.listen(3000, function(req,res){
-// 	console.log("The Server has Started!")	   
-// });
 	
